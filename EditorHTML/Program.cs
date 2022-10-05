a4876918b3f48b60ac6457c9f1be8b6bd145874d
1-LEI-DE-NEWTON <93740768+1-LEI-DE-NEWTON@users.noreply.github.com>
2022-10-05 12:15:53 -0300

    Create Program.cs

---
+++ EditorHTML/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace EditorHtml
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.Show();
+        }
+    }
+    
+}
